64e08b88d60e968e9df5f0498e2b16e02ce99fcd
melickas <demerigo@gmail.com>
2026-09-02 23:27:32 -0600

    SIWEBGCA V1.0

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react"
+
+function App() {
+
+  const [usuario, setusuario] = useState('')
+  const [psswd, setpsswd] = useState('')
+  const [logueado, setLogueado] = useState(false)
+
+  function boton() {
+    if (usuario === "Melissa" && psswd === "77237505") {
+      alert('El usuario es correcto')
+      setLogueado(true)
+    }
+    else {
+      alert('Usuario INVALIDO, intente de nuevo')
+    }
+  }
+
+  if (logueado) {
+    return (
+      <div>
+        <h1>Bienvenido al SIWEBGCA  {usuario}</h1>
+        <p>Has iniciado sesión correctamente.</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>SISTEMA WEB DE GESTIÓN DE CALIFICACIONES</h2>
+
+      <h4>Colegio de las Américas</h4>
+
+      <p>Bienvenidos</p>
+
+      <label>USUARIO:</label>
+
+      <input
+        type="text"
+        value={usuario}
+        onChange={(e) => setusuario(e.target.value)}
+      />
+
+      <br /><br />
+
+      <label>CONTRASEÑA:</label>
+
+      <input
+        type="password"
+        value={psswd}
+        onChange={(e) => setpsswd(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={boton}>ENTRAR</button>
+
+    </div>
+  )
+}
+
+export default App
